557911c3f0c1aec44821268280ff0ba2602a35e8
Jeremy Anderson <jande081@fiu.edu>
2018-03-27 17:54:08 -0400

    First commit

---
+++ function.js
@@ -0,0 +1,17 @@
+var a = 3
+
+function getUser(id, name, email){
+  var id = id
+  var name = name
+  var email = email
+
+  console.log(`This users id ${id}...`)
+}
+
+function sendBack(x){
+  return x * 3
+}
+
+// getUser("AB1234", "Jeremy", "janderson@techlaunch")
+
+var b = sendBack(a)
---
+++ index.js
@@ -0,0 +1,58 @@
+var n1 = 20 + 4;
+var n2 = 30 - 6;
+var n3 = 6 * 4;
+var n4 = 48 / 2;
+
+
+// console.log(n4)
+
+var op1 = 6 % 3
+var op2 = 10 % 2
+var op3 = 5 % 2
+var op4 = 72 % 10
+
+// console.log(op1 > op2)
+// console.log(op1 == op2)
+
+// console.log(op4)
+
+var num = 5
+num++
+num -= 3
+num--
+num *= 10
+// console.log(num)
+
+var str1 = "Test Grades as follows: \n "
+var str2 = "\t Student 1: 96 \n"
+var str3 = "\t Comment: \"None\""
+
+// console.log(str1 + str2 + str3)
+
+var stockPrice = 60.95
+// console.log(`The stock price is ${stockPrice}`)
+
+var numbers = [2, 10, 35, 42, 47, 55, 64, 79, 99, 106]
+console.log(numbers[2] + numbers[4])
+
+var me = {
+  first: "Jeremy",
+  last: "Anderson",
+  age: 30,
+  food: "Steak",
+  whiskey: "Glenlivet"
+}
+
+// console.log(`Hi my name is ${me.first} ${me.last}. I am ${me.age} years old. I like a good ${me.whiskey} on the rocks with my ${me.food}.`)
+
+// var numm = 1
+// while(numm <=10){
+//   if(numm % 2==0){
+//     console.log(numm)
+//   }
+//   numm++
+// }
+
+for(var i=1; i<10; i+=2){
+    console.log(i)
+}
